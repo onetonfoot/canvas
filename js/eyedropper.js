c99976858b849099652ed43640164ad8fbb6f6cd
poonyufung <poonyufung@gmail.com>
2017-10-30 10:05:03 +0800

    modified eyedropper

--- js/eyedropper.js
+++ js/eyedropper.js
@@ -12,16 +12,17 @@ class Eyedropper extends PaintFunction{
     }
     onMouseMove(coord,event){
         var pxData = this.context.getImageData(coord[0],coord[1], 1, 1);
-        console.log($("#strokecolor").spectrum("get"));
         if (this.selected == false) {
         $(".strokespectrum > div > div").css("background", "rgb(" + pxData.data[0] + "," + pxData.data[1] + "," + pxData.data[2] + ")");
         }
     }
     onMouseDown(coord,event){
         var pxData = this.context.getImageData(coord[0],coord[1], 1, 1);
+        if (this.selected == false) {
         $("#strokecolor").spectrum("set", "rgb(" + pxData.data[0] + "," + pxData.data[1] + "," + pxData.data[2] + ")");
         this.selected = true;
         $(".strokespectrum").css("background", "#EEE");
+        }
         
     }
     onMouseUp(coord,event){}
